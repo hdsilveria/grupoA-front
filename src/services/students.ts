13e93cdea7c46f28621aa70aebf39c2483731d2b
Henrique <hdsilveria@huia.haus>
2022-10-19 00:48:36 -0300

    create services to consult students;

--- src/services/students.ts
+++ src/services/students.ts
@@ -3,6 +3,14 @@ import type { IStudent } from '@/models/student'
 
 export default {
     async getStudents(): Promise<any>{
-        return api.get<IStudent>('/students')
+        return api.get<IStudent>('students')
+    },
+
+    async findStudent(name: string): Promise<any>{
+        return api.get(`students/student?name=${name}`)
+    },
+
+    async findStudentById(id: number): Promise<any>{
+        return api.get(`students/${id}`)
     }
 }
