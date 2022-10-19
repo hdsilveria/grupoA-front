7b3062692dd9e6db1838026524a61e6b8653feb4
Henrique <hdsilveria@huia.haus>
2022-10-19 00:48:51 -0300

    create request to filter student;

--- src/components/students/Filter.vue
+++ src/components/students/Filter.vue
@@ -2,15 +2,59 @@
     <header class="filter">
         <h1 class="filter__title">Consulta de alunos</h1>
         <div class="filter__content">
-            <v-autocomplete label="Digite sua busca" variant="underlined"></v-autocomplete>
+            <v-autocomplete 
+            label="Digite sua busca" 
+            variant="underlined"
+            :items="listStudent"
+            :loading="isLoading"
+            v-model="student"
+            v-model:search="searchNameStudent"
+            :item-title="'name'"
+            return-object
+            ></v-autocomplete>
         </div>
     </header>
 </template>
 
 <script lang="ts">
 
-export default {
+import studantService from '@/services/students'
 
+export default {
+    data(){
+        return {
+            searchNameStudent: '',
+            student: [],
+            listStudent: [],
+            isLoading: false,
+            itemsStudent: []
+        }
+    },
+
+    created(){
+        this.getStudent('')
+    },
+
+    watch: {
+        searchNameStudent(val){
+            this.getStudent(val)
+        },
+
+        student(val){
+            console.log(val.id)
+            this.$router.push({name: 'student', params: {id: val.id}})
+        }
+    },
+
+    methods: {
+        async getStudent(name: string | any): Promise<any>{
+            this.isLoading = true
+            studantService.findStudent(name).then(res => {
+                this.listStudent = res.data
+                this.isLoading = false
+            })
+        }   
+    },
 }
 
 </script>
